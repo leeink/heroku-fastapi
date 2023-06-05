a492a9d548b4c11c9db1674b5fcadce056acb3d4
leeink <zzxc78211@gmail.com>
2023-06-05 22:33:24 +0900

    modify apikey

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from models import Question
 
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
 MODEL = 'gpt-3.5-turbo'
 openai.api_key = OPENAI_API_KEY
 
@@ -16,23 +16,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-def get_response(question, prompt):
-    response = openai.Completion.create(
-        model=MODEL,
-        messages = [
-        {"role": "system", "content": prompt},
-        {"role": "user", "content": question}
-        ]
-    )
-    answer = response.choices[0]['message']['content']
-    return answer
-
 @app.post("/question")
 def read_root(params: Question):
     question = params.question
     prompt = params.feature
-    #print(question, prompt)
+    print(question, prompt)
     res = openai.ChatCompletion.create(
     model= MODEL,
     messages=[
