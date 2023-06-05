b814fd3c1945bb425167b3392482173674702564
leeink <zzxc78211@gmail.com>
2023-06-05 22:21:58 +0900

    add LLM API

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import openai
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from models import Question
+
+OPENAI_API_KEY = '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'
+MODEL = 'gpt-3.5-turbo'
+openai.api_key = OPENAI_API_KEY
 
 app = FastAPI()
 app.add_middleware(
@@ -11,12 +16,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post('/question')
-def sendquestion(question: Question):
-    try:
-        # generating answer for question
-        question = question.question
-        res = "answer the question"
-    except HTTPException as e:
-        print("Question not found")
-    return {'result': res}
+
+def get_response(question, prompt):
+    response = openai.Completion.create(
+        model=MODEL,
+        messages = [
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": question}
+        ]
+    )
+    answer = response.choices[0]['message']['content']
+    return answer
+
+@app.post("/question")
+def read_root(params: Question):
+    question = params.question
+    prompt = params.feature
+    #print(question, prompt)
+    res = openai.ChatCompletion.create(
+    model= MODEL,
+    messages=[
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": question},
+        ]
+    )
+
+    answer = res.choices[0]['message']['content']
+    return {"answer": answer}
